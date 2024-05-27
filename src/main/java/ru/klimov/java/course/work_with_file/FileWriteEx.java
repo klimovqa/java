e836a28e70dbfc20ac22d19956ce6b1b410249ec
Medved <mklimov2401@gmail.com>
2024-05-27 21:44:44 +0300

    FileWriter

---
+++ src/main/java/ru/klimov/java/course/work_with_file/FileWriteEx.java
@@ -0,0 +1,33 @@
+package ru.klimov.java.course.work_with_file;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
+
+public class FileWriteEx {
+    public static void main(String[] args) throws IOException {
+        String text = """
+                Октябрь уж наступил — уж роща отряхает
+                Последние листы с нагих своих ветвей;
+                Дохнул осенний хлад — дорога промерзает.
+                Журча еще бежит за мельницу ручей,
+                Но пруд уже застыл; сосед мой поспешает
+                В отъезжие поля с охотою своей,
+                И страждут озими от бешеной забавы,
+                И будит лай собак уснувшие дубравы.
+                """;
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter("C:\\Users\\Medved\\Downloads\\text.txt");
+            //for (int i = 0; i < text.length(); i++) {
+                //fileWriter.write(text.charAt(i));
+                fileWriter.write(text);
+           // }
+            System.out.println("Done!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Objects.requireNonNull(fileWriter).close();
+        }
+    }
+}
