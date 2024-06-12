7249b78690459899fed905afb779b2261993b745
MKlimov <mklimov2401@gmail.com>
2024-06-12 21:42:56 +0300

    PathAndFiles

---
+++ src/main/java/ru/klimov/java/course/work_with_file/PathAndFilesEx1.java
@@ -0,0 +1,36 @@
+package ru.klimov.java.course.work_with_file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathAndFilesEx1 {
+    public static void main(String[] args) throws IOException {
+        Path filePath = Paths.get("test15.txt");
+        Path directoryPath = Paths.get("C:\\Users\\Medved\\YandexDisk\\directory");
+        System.out.println("filePath.getFileName() " + filePath.getFileName());
+        System.out.println("directoryPath.getFileName() " + directoryPath.getFileName());
+        System.out.println("________________________________");
+        System.out.println("filePath.getParent() " + filePath.getParent());
+        System.out.println("directoryPath.getParent() " + directoryPath.getParent());
+        System.out.println("________________________________");
+        System.out.println("filePath.getRoot() " + filePath.getRoot());
+        System.out.println("directoryPath.getRoot() " + directoryPath.getRoot());
+        System.out.println("________________________________");
+        System.out.println("filePath.isAbsolute() " + filePath.isAbsolute());
+        System.out.println("directoryPath.isAbsolute() " + directoryPath.isAbsolute());
+        System.out.println("________________________________");
+        System.out.println("filePath.toAbsolutePath() " + filePath.toAbsolutePath());
+        System.out.println("directoryPath.toAbsolutePath() " + directoryPath.toAbsolutePath());
+        System.out.println("________________________________");
+
+
+        if (!Files.exists(filePath)){
+            Files.createFile(filePath);
+        }
+        if (!Files.exists(directoryPath)){
+            Files.createDirectory(directoryPath);
+        }
+    }
+}
