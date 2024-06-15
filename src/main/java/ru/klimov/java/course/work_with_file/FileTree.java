cd8528ddcd59b677eb7d871e667e12f49da91d5d
MKlimov <mklimov2401@gmail.com>
2024-06-15 10:53:57 +0300

    walkFileTree

---
+++ src/main/java/ru/klimov/java/course/work_with_file/FileTree.java
@@ -0,0 +1,39 @@
+package ru.klimov.java.course.work_with_file;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileTree {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\Users\\Medved\\Desktop\\result");
+        Files.walkFileTree(path, new MyFileVisitor());
+    }
+}
+
+class MyFileVisitor implements FileVisitor<Path> {
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println("Enter to directory: " + dir);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println("File name: " + file.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error while visiting file: " + file.getFileName());
+        return FileVisitResult.TERMINATE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("Post visit directory: " + dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
