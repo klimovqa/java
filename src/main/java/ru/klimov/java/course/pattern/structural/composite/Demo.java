f1d223f980bc018ed3362d1a0312eed923452062
MKlimov <mklimov2401@gmail.com>
2024-08-13 21:52:15 +0300

    pattern composite

---
+++ src/main/java/ru/klimov/java/course/pattern/structural/composite/Demo.java
@@ -0,0 +1,32 @@
+package ru.klimov.java.course.pattern.structural.composite;
+
+import ru.klimov.java.course.pattern.structural.composite.editor.ImageEditor;
+import ru.klimov.java.course.pattern.structural.composite.shapes.Circle;
+import ru.klimov.java.course.pattern.structural.composite.shapes.CompoundShape;
+import ru.klimov.java.course.pattern.structural.composite.shapes.Dot;
+import ru.klimov.java.course.pattern.structural.composite.shapes.Rectangle;
+
+import java.awt.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
